34b366b11f183dca96a8410e4884505532432fe3
Houssam El-Mossaid Idrissi <hoel-mos@c4r8p1.1337.ma>
2025-06-10 21:10:59 +0100

    1s push

---
+++ helpers.c
@@ -0,0 +1,34 @@
+#include "philosophers.h"
+
+
+// Get current time in milliseconds
+long get_current_time_ms() {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return (tv.tv_sec * 1000) + (tv.tv_usec / 1000);
+}
+
+// Get elapsed time since program start
+long get_elapsed_time(long start_time) {
+    return get_current_time_ms() - start_time;
+}
+
+// Sleep for specified milliseconds
+void precise_sleep(int milliseconds) {
+    usleep(milliseconds * 1000); // usleep takes microseconds
+}
+
+long long ft_atoi(char *str)
+{
+	int i;
+	long long result;
+
+	i = 0;
+	result = 0;
+	while(str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - 48);
+		i++;
+	}
+	return (result);
+}
---
+++ philosophers.h
@@ -0,0 +1,36 @@
+#ifndef PHILOSOPHERS_H
+#define PHILOSOPHERS_H
+
+#include <stdio.h>       // printf()
+#include <stdlib.h>      // malloc(), free(), atoi()
+#include <pthread.h>     // pthread functions and types
+#include <sys/time.h>    // gettimeofday(), struct timeval
+#include <unistd.h>      // usleep()
+
+typedef struct s_data
+{
+	long long p_n;
+	long long t_d;
+	long long t_s;
+	long long t_e;
+	int meals;
+	int number_of_meals;
+	int start;
+	pthread_mutex_t *forks;
+} t_data;
+
+typedef struct s_philosopher {
+    int id;                    // Philosopher number (0, 1, 2, 3, 4)
+    int left_fork_id;          // Which fork is on their left
+    int right_fork_id;         // Which fork is on their right
+    long last_meal_time;       // When they last started eating
+    int meals_eaten;           // How many times they've eaten
+    t_data *shared_data;       // Pointer to shared program data
+} t_philosopher;
+
+
+long long ft_atoi(char *str);
+void launch(pthread_t *philos, t_philosopher *philo_struct, t_data *data);
+
+
+#endif
