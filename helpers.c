881718e85a6038ee1c6df71a5325b4769dc25598
Houssam El-Mossaid Idrissi <hoel-mos@c4r7p3.1337.ma>
2025-07-12 17:38:11 +0100

    1.2st-prepush push

--- helpers.c
+++ helpers.c
@@ -1,47 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   helpers.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hoel-mos <hoel-mos@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/12 17:11:07 by hoel-mos          #+#    #+#             */
+/*   Updated: 2025/07/12 17:18:16 by hoel-mos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philosophers.h"
 
-
-// Get current time in milliseconds
-long get_current_time_ms()
+void	printing(int check, t_philosopher *philo)
 {
-    struct timespec ts;
-    clock_gettime(CLOCK_MONOTONIC, &ts);
-    return (ts.tv_sec * 1000L) + (ts.tv_nsec / 1000000L);
-}
+	long	start;
 
-long el_time(long start_time)
-{
-    return (get_current_time_ms() - start_time);
-}
-
-void printing(int check, t_philosopher *philo)
-{
+	start = philo->shared_data->start;
 	pthread_mutex_lock(&philo->shared_data->printing_mutex);
-    if(check == 3)
-		printf("[%ld]: %d died\n", el_time(philo->shared_data->start), philo->id);
-	else if(check == 7)
-        printf("[%ld]: %d has taken two forks\n", el_time(philo->shared_data->start), philo->id);
-    else if(check == 1)
-	    printf("[%ld]: %d is sleeping\n", el_time(philo->shared_data->start), philo->id);
-    else if(check == 2)
-        printf("[%ld]: %d is eating\n", el_time(philo->shared_data->start), philo->id);
-    else if(check == 6)
-        printf("[%ld]: %d has taken one fork\n", el_time(philo->shared_data->start), philo->id);
-	else if(check == 4)
-        printf("[%ld]: all the philos ate their meals\n", el_time(philo->shared_data->start));
-	else if(check == 5)
-        printf("[%ld]: %d is thinking\n", el_time(philo->shared_data->start), philo->id);
-    pthread_mutex_unlock(&philo->shared_data->printing_mutex);
+	if (check == 3)
+		printf("[%ld]: %d died\n", el_time(start), philo->id);
+	else if (check == 7)
+		printf("[%ld]: %d has taken two forks\n", el_time(start), philo->id);
+	else if (check == 1)
+		printf("[%ld]: %d is sleeping\n", el_time(start), philo->id);
+	else if (check == 2)
+		printf("[%ld]: %d is eating\n", el_time(start), philo->id);
+	else if (check == 6)
+		printf("[%ld]: %d has taken one fork\n", el_time(start), philo->id);
+	else if (check == 4)
+		printf("[%ld]: all the philos ate their meals\n", el_time(start));
+	else if (check == 5)
+		printf("[%ld]: %d is thinking\n", el_time(start), philo->id);
+	pthread_mutex_unlock(&philo->shared_data->printing_mutex);
 }
-// ask  if some cases must be handled while working with atoi
-long long ft_atoi(char *str, long long *var)
+
+long long	ft_atoi(char *str, long long *var)
 {
-	int i;
-	long long result;
+	int			i;
+	long long	result;
 
 	i = 0;
 	result = 0;
-	while(str[i] >= '0' && str[i] <= '9')
+	while (str[i] >= '0' && str[i] <= '9')
 	{
 		result = result * 10 + (str[i] - 48);
 		i++;
@@ -52,8 +53,11 @@ long long ft_atoi(char *str, long long *var)
 
 int	should_die(t_philosopher *philo)
 {
+	long long	t_d;
+
+	t_d = philo->shared_data->t_d;
 	pthread_mutex_lock(&philo->meal_mutex);
-	if (get_current_time_ms() - philo->last_meal_time >= philo->shared_data->t_d)
+	if (get_current_time_ms() - philo->last_meal_time >= t_d)
 	{
 		pthread_mutex_unlock(&philo->meal_mutex);
 		printing(3, philo);
@@ -68,10 +72,8 @@ int	should_die(t_philosopher *philo)
 
 int	ate_enough(t_philosopher *philo)
 {
-
 	if (philo->shared_data->meals_to_eat <= 0)
 		return (0);
-
 	if (philo->shared_data->meals >= philo->shared_data->p_n)
 	{
 		printing(4, philo);
@@ -82,30 +84,3 @@ int	ate_enough(t_philosopher *philo)
 	}
 	return (0);
 }
-
-
-// int ate_enough(t_philosopher   *philo)
-// {
-//     if (philo->shared_data->meals == philo->shared_data->p_n)
-//     {
-//         philo->shared_data->done_time = get_current_time_ms() - philo->shared_data->start;
-//         pthread_mutex_lock(&philo->shared_data->check_mutex); 
-//         philo->shared_data->check = true;
-//         pthread_mutex_unlock(&philo->shared_data->check_mutex);
-//         return (1);
-//     }
-//     return (0);
-// }
-
-// int should_die(t_philosopher   *philo)
-// {
-//     philo->shared_data->death_time = get_current_time_ms() - philo->shared_data->start;
-//     if (philo->shared_data->death_time >= philo->shared_data->t_d)
-//     {
-//         pthread_mutex_lock(&philo->shared_data->check_mutex); 
-//         philo->shared_data->check = true;
-//         pthread_mutex_unlock(&philo->shared_data->check_mutex);
-//         return (1);
-//     }
-//     return (0);
-// }
--- philosophers.h
+++ philosophers.h
@@ -1,58 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hoel-mos <hoel-mos@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/12 17:30:18 by hoel-mos          #+#    #+#             */
+/*   Updated: 2025/07/12 17:33:54 by hoel-mos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PHILOSOPHERS_H
-#define PHILOSOPHERS_H
+# define PHILOSOPHERS_H
 
-#define _GNU_SOURCE
-#include <stdio.h>       // printf()
-#include <stdlib.h>      // malloc(), free(), atoi()
-#include <pthread.h>     // pthread functions and types
-#include <sys/time.h>    // gettimeofday(), struct timeval
-#include <unistd.h>      // usleep()
-#include <stdbool.h>
-// #include <time.h>
-// #include <linux/time.h>
+# define _GNU_SOURCE
 
+# include <stdio.h>
+# include <stdlib.h>
+# include <pthread.h>
+# include <sys/time.h>
+# include <unistd.h>
+# include <stdbool.h>
 
 typedef struct s_data
 {
-    int     meals;
-	bool check;
-    bool one;
-	long long p_n; // number of philosophers
-	long long t_d; // time to die
-	long long t_s; // time to sleep
-	long long t_e; // time to eat
-	int meals_to_eat;
-	long long tmp;
-	long start;
-	pthread_mutex_t *forks;
-	pthread_mutex_t check_mutex;       // Pointer to shared program data
-	pthread_mutex_t printing_mutex;
-} t_data;
+	int					meals;
+	bool				check;
+	bool				one;
+	long long			p_n;
+	long long			t_d;
+	long long			t_s;
+	long long			t_e;
+	int					meals_to_eat;
+	long long			tmp;
+	long				start;
+	pthread_mutex_t		*forks;
+	pthread_mutex_t		check_mutex;
+	pthread_mutex_t		printing_mutex;
+}						t_data;
 
-typedef struct s_philosopher {
-    int id;                    // Philosopher number (0, 1, 2, 3, 4)
-    int left_fork_id;          // Which fork is on their left
-    int right_fork_id;         // Which fork is on their right
-    long last_meal_time;       // When they last started eating
-    int meals_eaten;
-    t_data *shared_data;
-	pthread_mutex_t meal_mutex;       // Pointer to shared program data
-} t_philosopher;
+typedef struct s_philosopher
+{
+	int					id;
+	int					left_fork_id;
+	int					right_fork_id;
+	long				last_meal_time;
+	int					meals_eaten;
+	t_data				*shared_data;
+	pthread_mutex_t		meal_mutex;
+}						t_philosopher;
 
-void cleanup(t_data *data, t_philosopher *philos, pthread_t *threads);
-long get_current_time_ms();
-long long ft_atoi(char *str, long long *var);
-void launch(pthread_t *philos, t_philosopher *philo_struct, t_data *data);
-void sleep_func( t_philosopher* philo);
-void think( t_philosopher* philo);
-void eat( t_philosopher* philo);
-void lock_forks(t_philosopher* philo );
-void unlock_forks(t_philosopher* philo );
-int should_die(t_philosopher   *philo);
-int ate_enough(t_philosopher   *philo);
-t_data *data();
-void ft_usleep(long long duration, t_philosopher *philo);
-void printing(int check, t_philosopher *philo);
-
+void		cleanup(t_data *data, t_philosopher *philos, pthread_t *threads);
+long		get_current_time_ms(void);
+long long	ft_atoi(char *str, long long *var);
+void		launch(pthread_t *philos,
+				t_philosopher *philo_struct, t_data *data);
+void		sleep_func(t_philosopher *philo);
+void		think(t_philosopher *philo);
+void		eat(t_philosopher *philo);
+void		lock_forks(t_philosopher *philo);
+void		unlock_forks(t_philosopher *philo);
+int			should_die(t_philosopher *philo);
+int			ate_enough(t_philosopher *philo);
+void		ft_usleep(long long duration, t_philosopher *philo);
+void		printing(int check, t_philosopher *philo);
+long		el_time(long start_time);
+t_data		*data(void);
 
 #endif
